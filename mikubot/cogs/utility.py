8821749abbea512e750ac465f332dc29bee8cad9
chisaku-dev <isaacto3890@gmail.com>
2021-01-27 00:42:26 -0800

    moved bugreport to utility.py

--- mikubot/cogs/utility.py
+++ mikubot/cogs/utility.py
@@ -40,6 +40,28 @@ class Utility(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(
+        name='bugreport',
+        help='Ask the creator of Miku bot for help!',
+        pass_context=True
+    )
+    async def support(self, ctx):
+        def check(ms):
+            return ms.channel == ctx.message.channel and ms.author == ctx.message.author
+        await ctx.send('Write what issues you have, this will be sent through Miku bot to the support channel in https://discord.gg/nQ4gNCtebV')
+        msg = await self.bot.wait_for('message', check=check)
+        message_embed = discord.Embed(
+                    color=0xE74C3C
+                    )
+        message_embed.set_footer(
+                        text=f'Sent by {msg.author.name} at {msg.author.guild.name}',
+                        icon_url=msg.author.avatar_url
+                    )
+        message_embed.description = msg.content
+        await self.bot.get_channel(794377032464334848).send(embed=message_embed)
+        await ctx.send("the message was sent successfully, If you need assistance, please join the server mentioned above. Support is offered voluntarily and nobody is forced to assist you in your perils.")
+    
     #embed generator
     @commands.command(
         name='embed',
