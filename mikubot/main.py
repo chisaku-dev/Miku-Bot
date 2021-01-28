3cbe57cc8ed24ea0f3850620ed512709198d45bf
chisaku-dev <isaacto3890@gmail.com>
2021-01-28 13:05:46 -0800

    Now saves bot tokens to sqlite3. Supports multiple

--- mikubot/main.py
+++ mikubot/main.py
@@ -2,7 +2,52 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
+import sqlite3
+#saving bot tokens in database
+def key():
+    try:
+        con = sqlite3.connect('./mikubot/bot.db')
+        con.execute('CREATE TABLE IF NOT EXISTS botkeys (botname VARCHAR(255) NOT NULL UNIQUE, key VARCHAR(255) NOT NULL UNIQUE)')
+        con.commit()
+        con.close()
+        print('table initialized')
+    except sqlite3.Error as e:
+        print('Error', e)
+    choices = ['add new bot', 'run existing bot']
+    while True:
+        for i in range(0, len(choices)):
+            print(i, choices[i])
+        chosentask = int(input("Enter what you want to do: "))
+        con = sqlite3.connect('./mikubot/bot.db')
+        cursor = con.cursor()
+        if chosentask == 0:
+            #add new bot
+            try:
+                name = input('What is the name of the bot? ')
+                key = input('What is the bot key? (This will be stored in the local database called "bot.db") ')
+                insert_sql = 'INSERT INTO botkeys (botname, key) VALUES (?, ?)'
+                con.execute(insert_sql, (name, key))
+                con.commit()
+                con.close()
+                return key
+            except sqlite3.Error as e:
+                print('Error', e)
+        elif chosentask == 1:
+            #run existing bot
+            try:
+                cursor.execute('SELECT botname FROM botkeys')
+                botnames = cursor.fetchall()
+                for bots in botnames:
+                    print(bots)
+                botname = input('Which bot do you want to run? Enter the name of the bot:')
+                cursor.execute('SELECT key FROM botkeys WHERE botname = ?', (botname,))
+                key = cursor.fetchone()
+                cursor.close()
+                return key
+            except sqlite3.Error as e:
+                print('Error', e)
 
+#start bot
 def get_prefix(client, message):
 
     prefixes = ['Miku ', 'miku ']
@@ -16,7 +61,7 @@ bot = commands.Bot(
     case_insensitive=True
 )
 
-TOKEN = input('Bot Token: ')
+TOKEN = key()
 
 cogs = ['cogs.description', 'cogs.basic', 'cogs.utility', 'cogs.fun', 'cogs.reddit', 'cogs.music', 'cogs.chess']
 
@@ -49,31 +94,34 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_message(message):
-    if not message.author.bot and message.channel.name == 'global':
-        for guild in bot.guilds:
-            for channel in guild.channels:
-                if channel.name == 'global' and channel.id != message.channel.id:
-                    message_embed = discord.Embed()
-                    message_embed.set_footer(
-                        text=f'Sent by {message.author.name} at {message.author.guild.name}',
-                        icon_url=message.author.avatar_url
-                    )
-                    if '.gif' in message.content or '.png' in message.content or '.jpg' in message.content:
-                        seperated = message.content.split(' ')
-                        strip = 0
-                        while strip != len(seperated):
-                            if '.gif' in seperated[strip] or '.png' in seperated[strip] or '.jpg' in seperated[strip]:
-                                break
-                            else:
-                                strip = strip + 1
-                        message_embed.set_image(url = str(seperated[strip]))
-                    try:
-                        message_embed.set_image(url = message.attachments[0].url)
-                    except:
-                        message_embed.set_image(url=None)
-                    message_embed.description = message.content
-                    await channel.send(embed=message_embed)
-    else:
-        await bot.process_commands(message)
+    try:
+        if not message.author.bot and message.channel.name == 'global':
+            for guild in bot.guilds:
+                for channel in guild.channels:
+                    if channel.name == 'global' and channel.id != message.channel.id:
+                        message_embed = discord.Embed()
+                        message_embed.set_footer(
+                            text=f'Sent by {message.author.name} at {message.author.guild.name}',
+                            icon_url=message.author.avatar_url
+                        )
+                        if '.gif' in message.content or '.png' in message.content or '.jpg' in message.content:
+                            seperated = message.content.split(' ')
+                            strip = 0
+                            while strip != len(seperated):
+                                if '.gif' in seperated[strip] or '.png' in seperated[strip] or '.jpg' in seperated[strip]:
+                                    break
+                                else:
+                                    strip = strip + 1
+                            message_embed.set_image(url = str(seperated[strip]))
+                        try:
+                            message_embed.set_image(url = message.attachments[0].url)
+                        except:
+                            message_embed.set_image(url=None)
+                        message_embed.description = message.content
+                        await channel.send(embed=message_embed)
+        else:
+            await bot.process_commands(message)
+    except:
+        await message.channel.send('Error: Global messaging only works in the global channel in a server')
 
 bot.run(TOKEN, bot=True, reconnect=True)
