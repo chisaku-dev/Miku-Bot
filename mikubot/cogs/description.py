206c46025845953d367b78604dcfe73041c216d5
chisaku-dev <isaacto3890@gmail.com>
2021-02-05 22:53:22 -0800

    reorganize description

--- mikubot/cogs/description.py
+++ mikubot/cogs/description.py
@@ -1,6 +1,11 @@
 from discord.ext import commands
 import discord
 
+helptext = '''Hey guys, I\'m the creator of Miku bot. I hope you have a good time playing with Miku!
+         Be sure to make a #global channel in your server to join the cross server chatroom!
+         You can join our support server at https://discord.gg/nQ4gNCtebV
+         or find any of my other accounts through https://sites.google.com/view/anivocaloid/link-redirection!'''
+
 class AnnouncementsInfo(commands.Cog, name="Announcements and Bot Info!"):
     
     def __init__(self, bot):
@@ -8,13 +13,13 @@ class AnnouncementsInfo(commands.Cog, name="Announcements and Bot Info!"):
         
     @commands.command(
         name='info',
-        help="Hey guys, I'm the creator of Miku bot. I hope you have a good time playing with Miku! Be sure to make a #global channel in your server to join the cross server chatroom! You can join our support server at https://discord.gg/nQ4gNCtebV or find any of my accounts through https://sites.google.com/view/anivocaloid/link-redirection!",
+        help=helptext,
         pass_context=True
         )
     async def support(self, ctx):
         info_embed = discord.Embed()
         info_embed.title='Bot Info'
-        info_embed.description='Hey guys, I\'m the creator of Miku bot. I hope you have a good time playing with Miku! Be sure to make a #global channel in your server to join the cross server chatroom! You can join our support server at https://discord.gg/nQ4gNCtebV or find any of my accounts through https://sites.google.com/view/anivocaloid/link-redirection!'
+        info_embed.description=helptext
         info_embed.set_thumbnail(url=self.bot.user.avatar_url)
         await ctx.send(embed = info_embed)
         return
