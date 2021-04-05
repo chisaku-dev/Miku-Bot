13537e115c4cc493be6b47a0bb313a0679e6a8f1
Chisaku-Dev <isaacto3890@gmail.com>
2021-04-05 01:52:14 -0700

    update support

--- mikubot/cogs/utility.py
+++ mikubot/cogs/utility.py
@@ -27,7 +27,7 @@ class Utility(commands.Cog):
                         icon_url=msg.author.avatar_url
                     )
         message_embed.description = msg.content
-        await self.bot.get_channel(794377032464334848).send(embed=message_embed)
+        await self.bot.get_channel(828552431721119795).send(embed=message_embed)
         await ctx.send("the message was sent successfully, If you need assistance, please join the server mentioned above. Support is offered voluntarily and nobody is forced to assist you in your perils.")
     
     #embed generator
