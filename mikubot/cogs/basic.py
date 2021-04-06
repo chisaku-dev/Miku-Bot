057c836094fc3e8f009a5e417484316c1ddc3769
Chisaku-Dev <isaacto3890@gmail.com>
2021-04-05 19:45:28 -0700

    baka update

--- mikubot/cogs/basic.py
+++ mikubot/cogs/basic.py
@@ -20,8 +20,11 @@ class Basic(commands.Cog):
         help='Ask miku to call someone a baka!',
         pass_context=True
     )
-    async def baka(self, ctx, arg):
-        await ctx.send(f"{arg} is a baka!")
+    async def baka(self, ctx, *arg: str):
+        if not arg:
+            await ctx.send(f"{ctx.message.author.mention} is a baka!")
+        else:
+            await ctx.send(f"{arg[0]} is a baka!")
         return
         
 def setup(bot):
